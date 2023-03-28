d91642e39972a1658caae97476e1f992fdbaee75
Yisan Huang <anonymousyisan@gmail.com>
2023-03-28 17:16:02 +0800

    add hires resize

--- main.py
+++ main.py
@@ -12,18 +12,24 @@ def get_timestamp():
 url = os.getenv("STABLE_DIFFUSION_API")
 
 payload = {
-    "prompt": "((masterpiece,best quality)),photon mapping, physically-based rendering, highly detailed background, high res, perspective,(1 girl),detailed clothes, white clothes, blunt bangs, braid, fine fabric emphasis,(large breasts:1.4), cleavage, wide-sleeved kimono, hair ornament, white japanese clothes,(bare leg:1.2), (pink hair:1.4),long hair, straight hair, detailed face, cool face, (smail:0.8),looking at viewer, beautiful eyes,detailed eyes, skirt, from side, (sea:1.4), (beach:0.9), sunshine,(hand between legs:1.7),standing,(ulzzang-6500:0.7),<lora:koreanDollLikeness_v15:0.4> <lora:yaeMikoRealistic_yaemikoMixed:0.7>",
+    "prompt": "((masterpiece, best quality)),close-up, facing viewer,straight on, face focus,((1 girl)), {long hair}, (medium breasts:1.4),(photo realistic:0.8), standing,  hair ornamen, gloves, (smile:1.2), (detailed light),lighting, colorful, layered backgrounds, (gorgeous background), dynamic angle, (shine),keqing \(genshin impact\),twintail, purple hair,(purple eyes:1.2),(outdoor),  physically-based rendering, RAW photo, highly detailed background, (photo realistic:0.8), high res, (black bodystocking),(potted plant:1.2),geyser, park, flower field,<lora:keqingGenshinImpact_10:0.75>  <lora:chilloutmixss_xss10:0.7>",
 
-    "negative_prompt": "EasyNegative, extra fingers, fewer fingers, nsfw, bad anatomy, low-res, (watermarks:1.2), username, paintings, sketches, (worst quality:2), (low quality:2), (normal quality:2), monochrome, grayscale, easynegative, ng_deepnegative_v1_75t, bad anatomy, low-res, poorly drawn face, disfigured hands, poorly drawn eyebrows, bad body perspective, animal tail, anime, nipples, pussy, wrong anatomy, poorly drawn legs, wrong perspective legs, extra legs,poorly drawn hands, (bad-hands-5:1.8), wrong hand",
+    "negative_prompt": "EasyNegative, extra fingers, fewer fingers, nsfw, bad anatomy, low-res, (watermarks:1.2), username, paintings, sketches, (worst quality:2), (low quality:2), (normal quality:2), monochrome, grayscale, easynegative, ng_deepnegative_v1_75t, bad anatomy, low-res, poorly drawn face, disfigured hands, poorly drawn eyebrows, bad body perspective, animal tail, anime, nipples, pussy, wrong anatomy, poorly drawn legs, wrong perspective legs, extra legs,poorly drawn hands, (bad-hands-5:1.8), wrong hand,EasyNegative, extra fingers, fewer fingers, (worst quality, low quality:1.4), loli",
     "seed": -1,
     "Model": "CounterfeitV25_25",
-    "cfg_scale": 6.5,
+    "cfg_scale": 4.5,
     "sampler_name": "DPM++ 2M Karras",
-    "width": 600,
-    "height": 900,
+    "width": 854,
+    "height": 480,
     "steps": 30,
-    "s_noise": 1337,
-    
+    "s_noise": 31337,
+    "enable_hr": True,
+    "hr_scale": 2,
+    "hr_upscaler": "Latent",
+    "hr_resize_x": 1280,
+    "hr_resize_y": 720,
+    "denoising_strength": 0.6,
+    "hr_second_pass_steps": 12,
 }
 
 def make_pic():
@@ -45,7 +51,7 @@ def make_pic():
         image.save("output/{}.png".format(get_timestamp()), pnginfo=pnginfo)
 
 def main():
-    for i in range(0,100):
+    for i in range(0,30):
         try:
             make_pic()
         except BaseException:
@@ -54,4 +60,3 @@ def main():
             
 
 main()
-
